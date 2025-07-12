b27e0d066ec54605aacec618e05a187249548b74
이동윤 <andy6609@idong-yun-ui-Macmini.local>
2025-07-12 19:03:41 +0100

    hello 10 times

--- hello/hello.go
+++ hello/hello.go
@@ -3,5 +3,7 @@ package main
 import "fmt"
 
 func main () {
-	fmt.Println("hello, world!") // fmt.print() -> unexported 
-}
+	for i:=0 ; i<10 ; i++ {                // it is not i = 0 . i := 0 is correct 
+		fmt.Println("hello, world!") // fmt.print() -> unexported 
+		}
+}	 
